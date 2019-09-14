868211bc7ec537b8a61288d6203760b36cf60b0a
Vincent Peng <vpeng@andrew.cmu.edu>
2019-09-14 02:48:54 -0400

    Create PersonBioViewController.swift

---
+++ search workout/search workout/PersonBioViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PersonBioViewController.swift
+//  search workout
+//
+//  Created by Vincent Peng on 9/14/19.
+//  Copyright © 2019 Vincent Peng. All rights reserved.
+//
+
+import UIKit
+
+class PersonBioViewController: UIViewController {
+
+    @IBOutlet weak var info: UIScrollView!
+    @IBOutlet weak var pic: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
