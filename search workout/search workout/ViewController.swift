d62309a7faf6eab9942fc3edb8a8516aa162ca63
Vincent Peng <vpeng@andrew.cmu.edu>
2019-09-14 11:33:48 -0400

    Update ViewController.swift

--- search workout/search workout/ViewController.swift
+++ search workout/search workout/ViewController.swift
@@ -10,17 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
     @IBOutlet weak var search: UISearchBar!
     @IBOutlet weak var titlebar: UINavigationBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+        let button = UIButton(frame: CGRect(x: 300, y:  150, width: 100, height: 50))
+        button.backgroundColor = .black
+        button.setTitle("Test Button", for: .normal)
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        
+        self.view.addSubview(button)
         // Do any additional setup after loading the view.
     }
-    /*override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    
+    @objc func buttonAction(sender: UIButton!) {
+        print("Button tapped")
+        //performSegue(withIdentifier: "toNextPage", sender: nil)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         view.endEditing(true)
         super.touchesBegan(touches, with: event)
-    }*/
+    }
+
+
+
     
 }
 
