857d05945a3a6ec4985b9c752773d7394543057b
Vincent Peng <vpeng@andrew.cmu.edu>
2019-09-14 02:48:51 -0400

    Create ViewController.swift

---
+++ search workout/search workout/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  search workout
+//
+//  Created by Vincent Peng on 9/14/19.
+//  Copyright © 2019 Vincent Peng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var search: UISearchBar!
+    @IBOutlet weak var titlebar: UINavigationBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    /*override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+        super.touchesBegan(touches, with: event)
+    }*/
+    
+}
+
